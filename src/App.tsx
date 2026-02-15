f18cd92f28b215d0404af74ae253785bb8b518c9
JavierTLoina <tejedajavier063@gmail.com>
2026-02-15 16:20:43 -0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,12 @@ import Footer from "./components/Footer";
 
 export default function App() {
   const [mode, setMode] = useState<"#121212" | "#F5F5F7">("#F5F5F7");
-  const [letters, setLetters] = useState<"Modo Oscuro" | "Modo Claro">(
-    "Modo Claro",
-  );
 
   const toggleMode = () => {
     if (mode === "#F5F5F7") {
       setMode("#121212");
-      setLetters("Modo Claro");
     } else {
       setMode("#F5F5F7");
-      setLetters("Modo Oscuro");
     }
   };
 
@@ -28,12 +23,7 @@ export default function App() {
       style={{ backgroundColor: mode }}
       className={`min-h-screen w-full transition-all duration-700 ease-in-out fade-in-page ${textColorClass} flex flex-col`}
     >
-      <Nav
-        mode={mode}
-        toggleMode={toggleMode}
-        letters={letters}
-        textColor={textColorClass}
-      />
+      <Nav mode={mode} toggleMode={toggleMode} textColor={textColorClass} />
       <Header />
       <main className="flex-1">
         <MainContent />
